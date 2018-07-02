283aeee77a19074c6afa00e989478756b52a92cd
Amr Adel Ata <amradelata@gmail.com>
2018-07-02 16:51:10 +0200

    Repair Clock

--- js/script.js
+++ js/script.js
@@ -1,46 +1,11 @@
-	function showTime () {
-		const date = new Date();
-		const h = date.getHours();
-		const m = date.getMinutes();
-		const s = date.getSeconds();
-		const session = "AM"
 
-		// if (h == 0){
-		// 	h = 12;
-		// }
-		// if (h > 12){
-		// 	h = h - 12; 
-		// 	session = "PM";
-		// }
 
-		// if(h < 10){
-		// 	h = `0 ${h}`;
-		// }
-		// if (m < 10){
-		// 	m = `0 ${m}`;
-		// }
-		// if(s < 10){
-		// 	s = `0 ${s}`;
-		// }
 
-		// h = (h < 10) ? "0" + h : h;
-		// m = (m < 10) ? "0" + m : m;
-		// s = (s < 10) ? "0" + s : s;
 
-		const time = `${h} : ${m} : ${s}   ${session}`   ;
+	const myConst = setInterval(myTimer, 1000);
 
-		document.getElementById('MyClockDisplay').innerHTML = time;
-
-		// setInterval(showTime, 1000)
-
-		console.log("fgdjhk")
+	function myTimer () {
+		const x = new Date();
+		const y = x.toLocaleTimeString();
+		document.getElementById("MyClockDisplay").innerHTML = y; 
 	}
-
-	showTime();
-
-	// setInterval(showTime, 1000)
-
-
-	// .innerText
-
-	//setInterval
